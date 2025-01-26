a47742a16840df7a55c9c591311aa88fefd068f5
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2025-01-26 13:09:57 +0100

    optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,17 @@ export default defineConfig({
             return "heroui";
           } else if (id.includes("@supabase/supabase-js")) {
             return "supabase";
+          } else if (id.includes("@octokit/rest")) {
+            return "octokit";
+          } else if (
+            id.includes("react-markdown") || id.includes("rehype-raw") ||
+            id.includes("remark-gfm")
+          ) {
+            return "markdown";
+          } else if (
+            id.includes("motion")
+          ) {
+            return "motion";
           }
         },
       },
